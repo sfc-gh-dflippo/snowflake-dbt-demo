8af74784cb1e16bd177eb2432ec7eccbbd5bdc67
Dan Flippo <dan.flippo@snowflake.com>
2024-01-26 11:16:51 -0500

    fix alter for iceberg with clustering

--- macros/iceberg_table_support.sql
+++ macros/iceberg_table_support.sql
@@ -3,6 +3,7 @@
 
   {#- Provide more generic support for a table_type parameter and add Iceberg support -#}
   {%- set table_type = config.get('table_type', default="") -%}
+  {%- set iceberg = (table_type.lower() == "iceberg") %}
   {% set iceberg_parameters = [
     ("external_volume", config.get('external_volume', default=none)),
     ("catalog", config.get('catalog', default=none)),
@@ -13,9 +14,14 @@
    ] %}
 
   {% if temporary and table_type == "" -%}
-    {%- set table_type = "temporary" -%}
+    {%- set create_table_type = "temporary" -%}
+    {%- set alter_table_type = "" -%}
   {%- elif transient and table_type == "" -%}
-    {%- set table_type = "transient" -%}
+    {%- set create_table_type = "transient" -%}
+    {%- set alter_table_type = "" -%}
+  {%- else -%}
+    {%- set create_table_type = table_type -%}
+    {%- set alter_table_type = table_type -%}
   {%- endif %}
 
   {%- if language == 'sql' -%}
@@ -35,14 +41,14 @@
 
     {{ sql_header if sql_header is not none }}
 
-        create or replace {{ table_type }} table {{ relation }}
+        create or replace {{ create_table_type }} table {{ relation }}
         {%- set contract_config = config.get('contract') -%}
         {%- if contract_config.enforced -%}
           {{ get_assert_columns_equivalent(sql) }}
           {{ get_table_columns_and_constraints() }}
           {% set compiled_code = get_select_subquery(compiled_code) %}
         {% endif %}
-        {% for parameter_name, parameter_value in iceberg_parameters if parameter_value is not none %}
+        {% for parameter_name, parameter_value in iceberg_parameters if parameter_value is not none -%}
           {{ parameter_name }} = '{{ parameter_value }}'
         {% endfor %}
         {% if copy_grants and not temporary -%} copy grants {%- endif %} as
@@ -56,10 +62,10 @@
           {%- endif %}
         );
       {% if cluster_by_string is not none and not temporary -%}
-        alter table {{relation}} cluster by ({{cluster_by_string}});
+        alter {{ alter_table_type }} table {{relation}} cluster by ({{cluster_by_string}});
       {%- endif -%}
       {% if enable_automatic_clustering and cluster_by_string is not none and not temporary  -%}
-        alter table {{relation}} resume recluster;
+        alter {{ alter_table_type }} table {{relation}} resume recluster;
       {%- endif -%}
 
   {%- elif language == 'python' -%}
