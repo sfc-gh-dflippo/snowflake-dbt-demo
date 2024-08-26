329e4c43f3ac8caaee12e74cc974328c61205733
Dan Flippo <dan.flippo@snowflake.com>
2024-08-26 13:26:39 -0400

    split largest macro into smaller macros for legibility

--- macros/snowflake_query_tags.sql
+++ macros/snowflake_query_tags.sql
@@ -8,85 +8,15 @@ https://github.com/get-select/dbt-snowflake-query-tags
 #}
 {% macro set_query_tag() -%}
 
-    {# Get session level query tag #}
-    {% set original_query_tag = get_current_query_tag() %}
-    {% set original_query_tag_parsed = {} %}
-    {% if original_query_tag %}
-        {% if fromjson(original_query_tag) is mapping %}
-            {% set original_query_tag_parsed = fromjson(original_query_tag) %}
-        {% else %}
-            {%- do original_query_tag_parsed.update(original_query_tag=original_query_tag) -%}
-        {% endif %}
-    {% endif %}
-
-    {# Get model config level query tag #}
-    {% set config_query_tag = config.get('query_tag') %}
-    {% set config_query_tag_parsed = {} %}
-    {% if config_query_tag %}
-        {% if fromjson(config_query_tag) is mapping %}
-            {% set config_query_tag_parsed = fromjson(config_query_tag) %}
-        {% else %}
-            {%- do config_query_tag_parsed.update(config_query_tag=config_query_tag) -%}
-        {% endif %}
-    {% endif %}
-
     {% set query_tag_dict = {} %}
-    {% do query_tag_dict.update(original_query_tag_parsed) %}
-    {% do query_tag_dict.update(config_query_tag_parsed) %}
+    {% do query_tag_dict.update(get_original_query_tag_parsed()) %}
+    {% do query_tag_dict.update(get_config_query_tag_parsed()) %}
 
     {% set env_det = {} %}
     {%- do env_det.update(
         database=target.database,
         schema=target.schema
     ) -%}
-
-    {% set run_det = {} %}
-    {%- do run_det.update(
-        full_refresh=flags.FULL_REFRESH,
-        which=flags.WHICH
-    ) -%}
-
-    {%- if env_var('DBT_CLOUD_PROJECT_ID', False) -%}
-        {%- do run_det.update(
-            dbt_cloud_project_id=env_var('DBT_CLOUD_PROJECT_ID')
-        ) -%}
-    {%- endif -%}
-
-    {%- if env_var('DBT_CLOUD_JOB_ID', False) -%}
-        {%- do run_det.update(
-            dbt_cloud_job_id=env_var('DBT_CLOUD_JOB_ID')
-        ) -%}
-    {%- endif -%}
-
-    {%- if env_var('DBT_CLOUD_RUN_ID', False) -%}
-        {%- do run_det.update(
-            dbt_cloud_run_id=env_var('DBT_CLOUD_RUN_ID')
-        ) -%}
-    {%- endif -%}
-
-    {%- if env_var('DBT_CLOUD_RUN_REASON_CATEGORY', False) -%}
-        {%- do run_det.update(
-            dbt_cloud_run_reason_category=env_var('DBT_CLOUD_RUN_REASON_CATEGORY')
-        ) -%}
-    {%- endif -%}
-
-    {%- if env_var('DBT_CLOUD_RUN_REASON', False) -%}
-        {%- do run_det.update(
-            dbt_cloud_run_reason=env_var('DBT_CLOUD_RUN_REASON')
-        ) -%}
-    {%- endif -%}
-
-    {%- if flags.INVOCATION_COMMAND -%}
-        {%- do run_det.update(
-            invocation_command=flags.INVOCATION_COMMAND
-        ) -%}
-    {%- endif -%}
-
-    {% if thread_id %}
-        {%- do run_det.update(
-            thread_id=thread_id
-        ) -%}
-    {% endif %}
 
     {%- do query_tag_dict.update(
         app='dbt',
@@ -96,7 +26,7 @@ https://github.com/get-select/dbt-snowflake-query-tags
         environment_details=env_det,
         run_id=invocation_id,
         run_started_at=run_started_at.astimezone(modules.pytz.utc).isoformat(),
-        run_details=run_det
+        run_details=get_run_details()
     ) -%}
 
     {%- if model -%}
@@ -160,6 +90,7 @@ https://github.com/get-select/dbt-snowflake-query-tags
     {{ return(original_query_tag)}}
 {% endmacro %}
 
+
 {% macro unset_query_tag(original_query_tag) -%}
     {% if original_query_tag %}
         {{ log("Resetting query_tag to '" ~ original_query_tag ~ "'.", info=false) }}
@@ -168,4 +99,89 @@ https://github.com/get-select/dbt-snowflake-query-tags
         {{ log("No original query_tag, unsetting parameter.", info=false) }}
         {% do run_query("alter session unset query_tag") %}
     {% endif %}
+{% endmacro %}
+
+
+{# Get session level query tag #}
+{% macro get_original_query_tag_parsed() -%}
+    {% set original_query_tag = get_current_query_tag() %}
+    {% set original_query_tag_parsed = {} %}
+    {% if original_query_tag %}
+        {% if fromjson(original_query_tag) is mapping %}
+            {% set original_query_tag_parsed = fromjson(original_query_tag) %}
+        {% else %}
+            {%- do original_query_tag_parsed.update(original_query_tag=original_query_tag) -%}
+        {% endif %}
+    {% endif %}
+
+    {{ return(original_query_tag_parsed)}}
+{% endmacro %}
+
+
+{# Get model config level query tag #}
+{% macro get_config_query_tag_parsed() -%}
+    {% set config_query_tag = config.get('query_tag') %}
+    {% set config_query_tag_parsed = {} %}
+    {% if config_query_tag %}
+        {% if fromjson(config_query_tag) is mapping %}
+            {% set config_query_tag_parsed = fromjson(config_query_tag) %}
+        {% else %}
+            {%- do config_query_tag_parsed.update(config_query_tag=config_query_tag) -%}
+        {% endif %}
+    {% endif %}
+
+    {{ return(config_query_tag_parsed)}}
+{% endmacro %}
+
+
+{% macro get_run_details() -%}
+    {% set run_det = {} %}
+    {%- do run_det.update(
+        full_refresh=flags.FULL_REFRESH,
+        which=flags.WHICH
+    ) -%}
+
+    {%- if env_var('DBT_CLOUD_PROJECT_ID', False) -%}
+        {%- do run_det.update(
+            dbt_cloud_project_id=env_var('DBT_CLOUD_PROJECT_ID')
+        ) -%}
+    {%- endif -%}
+
+    {%- if env_var('DBT_CLOUD_JOB_ID', False) -%}
+        {%- do run_det.update(
+            dbt_cloud_job_id=env_var('DBT_CLOUD_JOB_ID')
+        ) -%}
+    {%- endif -%}
+
+    {%- if env_var('DBT_CLOUD_RUN_ID', False) -%}
+        {%- do run_det.update(
+            dbt_cloud_run_id=env_var('DBT_CLOUD_RUN_ID')
+        ) -%}
+    {%- endif -%}
+
+    {%- if env_var('DBT_CLOUD_RUN_REASON_CATEGORY', False) -%}
+        {%- do run_det.update(
+            dbt_cloud_run_reason_category=env_var('DBT_CLOUD_RUN_REASON_CATEGORY')
+        ) -%}
+    {%- endif -%}
+
+    {%- if env_var('DBT_CLOUD_RUN_REASON', False) -%}
+        {%- do run_det.update(
+            dbt_cloud_run_reason=env_var('DBT_CLOUD_RUN_REASON')
+        ) -%}
+    {%- endif -%}
+
+    {%- if flags.INVOCATION_COMMAND -%}
+        {%- do run_det.update(
+            invocation_command=flags.INVOCATION_COMMAND
+        ) -%}
+    {%- endif -%}
+
+    {% if thread_id %}
+        {%- do run_det.update(
+            thread_id=thread_id
+        ) -%}
+    {% endif %}
+
+    {{ return(run_det)}}
 {% endmacro %}
