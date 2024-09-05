31791cff67dfb76470d982f63b8947681eb41b3c
Dan Flippo <dan.flippo@snowflake.com>
2024-09-05 09:00:22 -0400

    Added newer iceberg attributes

--- macros/iceberg_table_support.sql
+++ macros/iceberg_table_support.sql
@@ -1,17 +1,27 @@
+{# This macro is an update of the dbt-snowflake macro available at https://github.com/dbt-labs/dbt-snowflake/blob/main/dbt/include/snowflake/macros/relations/table/create.sql #}
 {% macro snowflake__create_table_as(temporary, relation, compiled_code, language='sql') -%}
   {%- set transient = config.get('transient', default=true) -%}
 
   {#- Provide more generic support for a table_type parameter and add Iceberg support -#}
   {%- set table_type = config.get('table_type', default="") -%}
   {%- set iceberg = (table_type.lower() == "iceberg") %}
-  {% set iceberg_parameters = [
+  {% set table_parameters = [
+    ("enable_schema_evolution", config.get('enable_schema_evolution', default=none)),
     ("external_volume", config.get('external_volume', default=none)),
     ("catalog", config.get('catalog', default=none)),
     ("catalog_table_name", config.get('catalog_table_name', default=none)),
     ("catalog_namespace", config.get('catalog_namespace', default=none)),
     ("metadata_file_path", config.get('metadata_file_path', default=none)),
-    ("base_location", config.get('base_location', default=none))
+    ("base_location", config.get('base_location', default=none)),
+    ("catalog_sync", config.get('catalog_sync', default=none)),
+    ("storage_serialization_policy", config.get('storage_serialization_policy', default=none)),
+    ("data_retention_time_in_days", config.get('data_retention_time_in_days', default=none)),
+    ("max_data_extension_time_in_days", config.get('max_data_extension_time_in_days', default=none)),
+    ("change_tracking", config.get('change_tracking', default=none)),
+    ("default_ddl_collation", config.get('default_ddl_collation', default=none))
    ] %}
+   {% set parameter_skips_quotes = ["enable_schema_evolution", "storage_serialization_policy", "data_retention_time_in_days",
+        "max_data_extension_time_in_days", "change_tracking"] %}
 
   {% if temporary and table_type == "" -%}
     {%- set create_table_type = "temporary" -%}
@@ -51,9 +61,15 @@
         {% if cluster_by_string is not none and not temporary -%}
         cluster by ( {{ cluster_by_string }} )
         {%- endif %}
-        {% for parameter_name, parameter_value in iceberg_parameters if parameter_value is not none -%}
-          {{ parameter_name }} = '{{ parameter_value }}'
-        {% endfor %}
+        {%- if not temporary -%}
+            {% for parameter_name, parameter_value in table_parameters if parameter_value is not none -%}
+            {%- if parameter_name in parameter_skips_quotes -%}
+                {{ parameter_name }} = {{ parameter_value }}
+            {%- else -%}
+                {{ parameter_name }} = '{{ parameter_value }}'
+            {%- endif %}
+            {% endfor %}
+        {%- endif %}
         {% if copy_grants and not temporary -%} copy grants {%- endif %} as
         (
           {%- if cluster_by_string is not none -%}
