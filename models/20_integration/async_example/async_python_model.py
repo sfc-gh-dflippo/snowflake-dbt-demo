ab920903b64d393faecb36e3badfbde968089ef3
Dan Flippo <dan.flippo@snowflake.com>
2025-05-29 16:53:59 -0400

    Updated to use native async sql

--- models/20_integration/async_example/async_python_model.py
+++ models/20_integration/async_example/async_python_model.py
@@ -1,30 +1,70 @@
-from joblib import Parallel, delayed
 import snowflake.snowpark as snowpark
+import time
 
 
-def async_bulk_query_writer(session: snowpark.Session, sql_statements, output_table_name, write_mode, table_type, commit_interval):
+def async_bulk_query_writer(session: snowpark.Session, sql_statements,
+        output_table_name, write_mode, table_type, commit_interval, parallel_query_limit):
     all_results = []
     num_results_found = 0
 
-    # Function to execute a SQL and wait for the results
-    get_query_result = lambda sql_statement: session.sql(sql_statement).collect()
+    running_processes = []
 
-    # Split up the work by the number of CPU processors
-    res = Parallel(n_jobs=-1, return_as="generator")(
-        delayed(get_query_result)(sql_statement) for sql_statement in sql_statements
-    )
+    # Submit each statement as an asyncronous job
+    for sql_statement in sql_statements:
 
-    # The generator will stream results to our all_results cache to minimize memory usage
-    for query_results in res:
-        all_results.extend( query_results )
+        # If we are at the query limit we need to see if a job has completed
+        # We won't leave this loop until we are below the query limit
+        while len(running_processes) == parallel_query_limit:
+            for index, async_job in enumerate(running_processes):
+                if async_job.is_done():
+                    # Remove the process from our list of running processes
+                    running_processes.pop(index)
+                    try:
+                        # Get results from a completed async job
+                        results = async_job.result()
+                        if results:
+                            all_results.extend(results)
+                    except:
+                        raise
 
-        # If we reach the commit interval, write the output
-        if len(all_results) >= commit_interval:
-            num_results_found += len(all_results)
-            session.createDataFrame(data=all_results).write.save_as_table(
-                table_name=output_table_name, mode=write_mode, table_type=table_type)
-            all_results = []
-            write_mode = "append"
+                    # If we reach the commit interval, write the output
+                    if len(all_results) >= commit_interval:
+                        num_results_found += len(all_results)
+                        session.createDataFrame(data=all_results).write.save_as_table(
+                            table_name=output_table_name, mode=write_mode, table_type=table_type)
+                        all_results = []
+                        write_mode = "append"
+
+            # Sleep if we still haven't completed a job
+            if len(running_processes) == parallel_query_limit:
+                time.sleep(0.1)
+
+        # Submit the additional async query to running processes
+        async_job = session.sql(sql_statement).collect_nowait()
+        running_processes.append(async_job)
+
+    # Wait for the final processes to end
+    while len(running_processes) > 0:
+        for index, async_job in enumerate(running_processes):
+            if async_job.is_done():
+                # Remove the process from our list of running processes
+                running_processes.pop(index)
+                try:
+                    # Get results from a completed async job
+                    results = async_job.result()
+                    if results:
+                        all_results.extend(results)
+                except:
+                    raise
+
+                # If we reach the commit interval, write the output
+                if len(all_results) >= commit_interval:
+                    num_results_found += len(all_results)
+                    session.createDataFrame(data=all_results).write.save_as_table(
+                        table_name=output_table_name, mode=write_mode, table_type=table_type)
+                    all_results = []
+                    write_mode = "append"
+
 
     # Write all the final results
     if len(all_results) > 0:
@@ -32,14 +72,13 @@ def async_bulk_query_writer(session: snowpark.Session, sql_statements, output_ta
         session.createDataFrame(data=all_results).write.save_as_table(
             table_name=output_table_name, mode=write_mode, table_type=table_type)
 
+    return f"Number of records written: {num_results_found}"
+
 
 def model(dbt, session: snowpark.Session):
-    dbt.config(materialized="table", packages = ["joblib"])
+    dbt.config(materialized="table")
     upload_cache_size = 100000
-    sql_statements = []
-    for x in range(10):
-        sql_statements.append(f"select {x} as row_num")
+    sql_statements = [ f"select {x} as row_num" for x in range(10) ]
+    async_bulk_query_writer(session, sql_statements, "MY_TEMP", "overwrite", "temporary", upload_cache_size, 10)
 
-    async_bulk_query_writer(session, sql_statements, "my_temp", "overwrite", "temporary", upload_cache_size)
-
-    return session.table("my_temp")
+    return session.table("MY_TEMP")
